ac5c707a593ed67e81d21cee66784001009d0816
Alberto Alejandro <alberto196g@gmail.com>
2020-09-10 01:18:06 -0400

    Finishing first part

--- app.js
+++ app.js
@@ -3,8 +3,56 @@ new Vue({
     data(){
         return {
             name: 'Bitcoin',
+            symbol: 'BTC',
             img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
             changePercent: 0,
+            // prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
+
+            priceOnly: 8400,
+
+            value: 0,
+
+            color: 'f4f4f4',
+
+            pricesWithDays: [
+                {day: 'Lunes', value: 8400},
+                {day: 'Martes', value: 7900},
+                {day: 'Miercoles', value: 8200},
+                {day: 'Jueves', value: 9000},
+                {day: 'Viernes', value: 9400},
+                {day: 'Sabado', value: 10000},
+                {day: 'Domingo', value: 10200},
+            ],
+
+            showPrices: false,
+        }
+    },
+
+    computed: {
+        title() {
+            return `${this.name} - ${this.symbol}`
+        },
+
+        convertedValue () {
+            if(!this.value){
+                console.log(this.value)
+                return 0
+            }
+            return this.value / this.priceOnly
+        }
+    },
+
+    watch: {
+        showPrices(newValue, oldValue){
+            console.log(newValue, oldValue)
+        }
+    },
+
+    methods: {
+        toggleShowPrices() {
+            this.showPrices = !this.showPrices
+
+            this.color = this.color.split('').reverse('').join('')
         }
     }
 })
